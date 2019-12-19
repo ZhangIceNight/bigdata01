bf08ba65710520fff13989e3031efca123444496
ZhangIceNight <51945786+ZhangIceNight@users.noreply.github.com>
2019-12-19 13:20:24 +0800

    2.py.success!!!!!

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
 from pyspark.sql.types import IntegerType
-
+from pyecharts import Bar
 filename = "/home/student/workspace/58/rent.csv"
 def spark_analyse(filename):
     print("开始spark分析")
@@ -62,5 +62,21 @@ def spark_analyse(filename):
     print(all_list)
     print("1111")
     return all_list
+def draw_bar(all_list):
+    print("开始绘图")
+    attr = ["南岗", "道里", "江北", "香坊", "道外", "松北", "平房"]
+    v0 = all_list[0]
+    v1 = all_list[1]
+    v2 = all_list[2]
+    v3 = all_list[3]
+
+    bar = Bar("harbin市租房租金概况")
+    bar.add("最小值", attr, v0, is_stack=True)
+    bar.add("最大值", attr, v1, is_stack=True)
+    bar.add("平均值", attr, v2, is_stack=True)
+    bar.add("中位数", attr, v3, is_stack=True)
+    bar.render()
+    print("结束绘图")
 if __name__=='__main__':
-    spark_analyse(filename)
+    all_list = spark_analyse(filename)
+    draw_bar(all_list)
